dc2484f3dc1c09655b7b67a2e001aa6a95f92dba
Allen Wang <allen.wang@yale.edu>
2017-12-03 04:26:35 +0000

    testing trimmed code

--- main.hh
+++ main.hh
@@ -31,157 +31,157 @@
 
 
 
-class MultiLineEdit : public QTextEdit
-{
-	Q_OBJECT
+// class MultiLineEdit : public QTextEdit
+// {
+// 	Q_OBJECT
 
-public:
-	MultiLineEdit(QWidget * parent = 0) : QTextEdit(parent) {}
+// public:
+// 	MultiLineEdit(QWidget * parent = 0) : QTextEdit(parent) {}
 
-signals:
-	void returnPressed();
+// signals:
+// 	void returnPressed();
 
-private:
-	void keyPressEvent(QKeyEvent *keyEvent) {
-		if(keyEvent->key()==Qt::Key_Return) {
-			qDebug() << "User Pressed Return" << endl;
-			emit MultiLineEdit::returnPressed();
-		}
-		else QTextEdit::keyPressEvent(keyEvent);
-	}
-
-
-
-};
-
-class ChatDialog : public QDialog
-{
-	Q_OBJECT
-
-public:
-	ChatDialog();
-
-	QTextEdit *getTextView();
-	MultiLineEdit *getTextLine();
-	MultiLineEdit *getAddPeersLine();
-	MultiLineEdit *getDownloadFileLine();
-	MultiLineEdit *getFileSearchLine();
-	MultiLineEdit *getJoinChordLine();
-	QListWidget *getFileSearchResultsList();
-	QPushButton *getShareFileButton();
-
-private:
-	QTextEdit *textview;
-	MultiLineEdit *textline;
-	MultiLineEdit *addPeersLine;
-	MultiLineEdit *downloadFileLine;
-	MultiLineEdit *fileSearchLine;
-	MultiLineEdit *joinChordLine;
-	QListWidget *fileSearchResultsList;
-	QPushButton *shareFileButton;
-};
-
-
-class NetSocket : public QUdpSocket
-{
-	Q_OBJECT
-
-public:
-	NetSocket();
-
-	// Bind this socket to a Peerster-specific default port.
-	bool bind();
-	int getMyPortMin();
-	int getMyPortMax();
-	int getMyPortVal();
-
-private:
-	int myPortMin, myPortMax;
-	int myPortVal;
-};
+// private:
+// 	void keyPressEvent(QKeyEvent *keyEvent) {
+// 		if(keyEvent->key()==Qt::Key_Return) {
+// 			qDebug() << "User Pressed Return" << endl;
+// 			emit MultiLineEdit::returnPressed();
+// 		}
+// 		else QTextEdit::keyPressEvent(keyEvent);
+// 	}
 
 
 
-class Peer
-{
+// };
 
-public:
-	Peer();
-	Peer(QHostAddress ipAddress, quint16 port);
-	Peer(QString hostName, QHostAddress ipAddress, quint16 port);
-	QString getHostName();
-	void setHostName(QString name);
-	QHostAddress getAddress();
-	void setAddress(QHostAddress address);
-	quint16 getPort();
-	void setPort(quint16 portNum);
+// class ChatDialog : public QDialog
+// {
+// 	Q_OBJECT
 
-private:
-	QString hostName;
-	QHostAddress ipAddress;
-	quint16 port;
-};
+// public:
+// 	ChatDialog();
 
+// 	QTextEdit *getTextView();
+// 	MultiLineEdit *getTextLine();
+// 	MultiLineEdit *getAddPeersLine();
+// 	MultiLineEdit *getDownloadFileLine();
+// 	MultiLineEdit *getFileSearchLine();
+// 	MultiLineEdit *getJoinChordLine();
+// 	QListWidget *getFileSearchResultsList();
+// 	QPushButton *getShareFileButton();
 
-// Main window class containing a ChatDialog and NetSocket
-class MessageSender : public QMainWindow
-{
-	Q_OBJECT
-
-public:
-	MessageSender();
-
-	QByteArray getSerialized(QVariantMap map);
-	QString getOriginID();
-	int getNeighbor(int val);
-	Peer getNeighbor();
-	void addPeer(QString input);
-	QVariantMap createBlockReply(QString dest, QString origin, QByteArray dataHash, QByteArray data);
-	QVariantMap createBlockRequest(QString dest, QString origin);
-	QVariantMap createBlockRequest(QString dest, QString origin, QByteArray dataHash);
-	QVariantMap createSearchRequest();
-	void sendToPeers(QByteArray data);
-	void handleStatusMessage(QVariantMap receivedMap, QHostAddress *senderAddress, quint16 *senderPort);
-	void handleRumorMessage(QVariantMap receivedMap, QHostAddress *senderAddress, quint16 *senderPort);
-	void handleBlockReplyMessage(QVariantMap receivedMap, QString senderOrigin);
-	void handleBlockRequestMessage(QVariantMap receivedMap, QString senderOrigin);
-	void handleSearchReplyMessage(QVariantMap receivedMap);
-	void localFileSearch(QString searchStr, QString dest);
-	void sendPointToPoint(QVariantMap map);
-	bool createFingerTable();
+// private:
+// 	QTextEdit *textview;
+// 	MultiLineEdit *textline;
+// 	MultiLineEdit *addPeersLine;
+// 	MultiLineEdit *downloadFileLine;
+// 	MultiLineEdit *fileSearchLine;
+// 	MultiLineEdit *joinChordLine;
+// 	QListWidget *fileSearchResultsList;
+// 	QPushButton *shareFileButton;
+// };
 
 
-public slots:
-	void gotReturnPressed();
-	void onReceive();
-	void peerLookup(QHostInfo host);
-	void addGuiPeer();
-	void startFileDownload(QListWidgetItem * listItem);
-	void openFileDialog();
-	void getFileMetadata(const QStringList &fileList);
-	void downloadFile();
+// class NetSocket : public QUdpSocket
+// {
+// 	Q_OBJECT
 
-private:
-	ChatDialog *chat;
-	NetSocket *socket;
-	QFileDialog *fileDialog;
-	QString originID;
-	quint32 nodeID;
-	QVariantMap msgMap;
-	QVariantMap fileHash;
-	QVariantMap fileMetadata;
-	QTimer *searchRequestTimer;
-	QVector<Peer> peerLst;
-	QVariantMap portMap;
-	QSet<QString> peerCheck;
-	QHash<QString, QPair<QHostAddress, quint16>> routeTable;
-	QByteArray fileReceiving;
-	QByteArray fileBuilder;
-	QString currentSearch;
-	QVariantMap searchResultsMap;
+// public:
+// 	NetSocket();
+
+// 	// Bind this socket to a Peerster-specific default port.
+// 	bool bind();
+// 	int getMyPortMin();
+// 	int getMyPortMax();
+// 	int getMyPortVal();
+
+// private:
+// 	int myPortMin, myPortMax;
+// 	int myPortVal;
+// };
+
+
+
+// class Peer
+// {
+
+// public:
+// 	Peer();
+// 	Peer(QHostAddress ipAddress, quint16 port);
+// 	Peer(QString hostName, QHostAddress ipAddress, quint16 port);
+// 	QString getHostName();
+// 	void setHostName(QString name);
+// 	QHostAddress getAddress();
+// 	void setAddress(QHostAddress address);
+// 	quint16 getPort();
+// 	void setPort(quint16 portNum);
+
+// private:
+// 	QString hostName;
+// 	QHostAddress ipAddress;
+// 	quint16 port;
+// };
+
+
+// // Main window class containing a ChatDialog and NetSocket
+// class MessageSender : public QMainWindow
+// {
+// 	Q_OBJECT
+
+// public:
+// 	MessageSender();
+
+// 	QByteArray getSerialized(QVariantMap map);
+// 	QString getOriginID();
+// 	int getNeighbor(int val);
+// 	Peer getNeighbor();
+// 	void addPeer(QString input);
+// 	QVariantMap createBlockReply(QString dest, QString origin, QByteArray dataHash, QByteArray data);
+// 	QVariantMap createBlockRequest(QString dest, QString origin);
+// 	QVariantMap createBlockRequest(QString dest, QString origin, QByteArray dataHash);
+// 	QVariantMap createSearchRequest();
+// 	void sendToPeers(QByteArray data);
+// 	void handleStatusMessage(QVariantMap receivedMap, QHostAddress *senderAddress, quint16 *senderPort);
+// 	void handleRumorMessage(QVariantMap receivedMap, QHostAddress *senderAddress, quint16 *senderPort);
+// 	void handleBlockReplyMessage(QVariantMap receivedMap, QString senderOrigin);
+// 	void handleBlockRequestMessage(QVariantMap receivedMap, QString senderOrigin);
+// 	void handleSearchReplyMessage(QVariantMap receivedMap);
+// 	void localFileSearch(QString searchStr, QString dest);
+// 	void sendPointToPoint(QVariantMap map);
+// 	bool createFingerTable();
+
+
+// public slots:
+// 	void gotReturnPressed();
+// 	void onReceive();
+// 	void peerLookup(QHostInfo host);
+// 	void addGuiPeer();
+// 	void startFileDownload(QListWidgetItem * listItem);
+// 	void openFileDialog();
+// 	void getFileMetadata(const QStringList &fileList);
+// 	void downloadFile();
+
+// private:
+// 	ChatDialog *chat;
+// 	NetSocket *socket;
+// 	QFileDialog *fileDialog;
+// 	QString originID;
+// 	quint32 nodeID;
+// 	QVariantMap msgMap;
+// 	QVariantMap fileHash;
+// 	QVariantMap fileMetadata;
+// 	QTimer *searchRequestTimer;
+// 	QVector<Peer> peerLst;
+// 	QVariantMap portMap;
+// 	QSet<QString> peerCheck;
+// 	QHash<QString, QPair<QHostAddress, quint16>> routeTable;
+// 	QByteArray fileReceiving;
+// 	QByteArray fileBuilder;
+// 	QString currentSearch;
+// 	QVariantMap searchResultsMap;
 	
-	QHash<QByteArray, QList<QByteArray>>* fingerTable;
-	QHash<QByteArray, QList<QByteArray>>* fileTable;
-};
+// 	QHash<QByteArray, QList<QByteArray>>* fingerTable;
+// 	QHash<QByteArray, QList<QByteArray>>* fileTable;
+// };
 
 #endif // PEERSTER_MAIN_HH
